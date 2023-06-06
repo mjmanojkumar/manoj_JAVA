181dd567c566c7784730ddf7571983bab7e22fda
mjmanojkumar <134974736+mjmanojkumar@users.noreply.github.com>
2023-06-06 11:13:22 -0500

    Added files of day 06

---
+++ src/day06/ReverseNumber.java
@@ -0,0 +1,20 @@
+package day06;
+
+public class ReverseNumber {
+
+    public static void main(String[] args){
+        int n = 12345;
+        int rev = 0;
+        int rem;
+
+        while(n>0){
+            rem = n%10;
+            rev = rev*10+rem;
+            n = n/10;
+
+        }
+        System.out.println(rev);
+
+    }
+
+}
